e5e6ab6b153d56c96e890f1f2be8aced3f79bba8
Yuwuyy <martiny@lacroixrouge-brest.fr>
2018-11-05 16:05:35 +0100

    Création de l'ihm de base, 4 boutons et 2 input text pour les deux moteurs

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Commande Robot</title>
+  </head>
+  <body>
+    <h1> commande robot </h1>
+<?php
+        echo "nous sommes aujourd'hui " . date("l"),",";
+	phpinfo();
+     ?>
+    </html>
+
+    
